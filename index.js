c55056e969d823b891c6dda5bc1de1323ab5a443
Nataliya R <n.rudenko@gmail.com>
2019-09-11 23:37:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // Code your solution in this file
-function findMatching(collection, name) {
-  for (const driver of collection) {
-    if (driver.name === name) {
-      console.log(driver.name);
-    }
-  }
+function findMatching(list, name) {
+ 
 }
