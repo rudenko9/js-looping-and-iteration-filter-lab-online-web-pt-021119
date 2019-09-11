83b7b202a3e2ce07f63c8cc155eaad41a7045daa
Nataliya R <n.rudenko@gmail.com>
2019-09-11 23:49:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(list, name) {
+ return list.filter(function (el) { return el.toUpperCase() === name.toUpperCase()});
+}
+
+function  fuzzyMatch(list, name) {
+  return list.filter(function(el) {return el[0] === name[0]});
+}
+
+function  matchName(list, name) {
+  return list.filter(function(el) {return el.name === name});
+}
+
+//function matchName(list, name) {
+ // return list.filter(el => el.name === name);
+//}
